1267f376c2966d39a753ebf4d1f74e921eba5069
12c-kucsera <12c-kucsera@H009-024009>
2025-05-22 12:35:42 +0200

    Add project files.

---
+++ Keresztrejtveny/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Keresztrejtveny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, orld!");
+        }
+    }
+}
